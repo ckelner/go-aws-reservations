028d77de772c7900d71fe6cc20acdb4c18c2549f
Chris Kelner <ckelner@users.noreply.github.com>
2017-02-21 15:02:01 -0500

    add version target to module usage

--- terraform/single-ec2-instance/main.tf
+++ terraform/single-ec2-instance/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "github.com/terraform-community-modules/tf_aws_vpc"
+  source = "github.com/terraform-community-modules/tf_aws_vpc?ref=v1.0.2"
   name = "kelnerhax"
   cidr = "10.0.0.0/16"
   public_subnets  = ["10.0.1.0/24"]
