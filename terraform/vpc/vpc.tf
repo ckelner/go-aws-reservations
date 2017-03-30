60c1f1e65877813f78500f8e200f78216df4bd7f
Chris Kelner <ckelner@users.noreply.github.com>
2017-03-30 09:25:03 -0400

    Add a security group

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -18,6 +18,13 @@ module "vpc" {
   }
 }
 
+module "sg_web" {
+  source = "github.com/terraform-community-modules/tf_aws_sg//sg_web"
+  security_group_name = "kelnerhax-web"
+  vpc_id = "${module.vpc.id}"
+  source_cidr_block = "0.0.0.0/0"
+}
+
 ##########################################################################
 ############# VARIABLES ##################################################
 ##########################################################################
